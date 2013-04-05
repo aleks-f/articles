e392f2c1211bad3de081b48541560cf1f6a43858
Aleksandar Fabijanic <alex@pocoproject.org>
2013-04-04 23:59:57 -0500

    removed non-Overload directories

--- ACCU-2013/DynamicC++/src/folly_dynamic.h
+++
@@ -1,9 +0,0 @@
-#ifndef DynamicCPP_folly_dynamic_included
-#define DynamicCPP_folly_dynamic_included
-
-#include <vector>
-#include <string>
-
-void doFollyDynamic(std::vector<std::string>);
-
-#endif // DynamicCPP_folly_dynamic_included
