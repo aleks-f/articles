3b44b4af3e69f8e2ed23e6188cfb4a19c85017b8
aleks-f <alex@pocoproject.org>
2013-04-04 22:43:29 -0500

    added folly:dynamic

---
+++ ACCU-2013/DynamicC++/src/folly_dynamic.h
@@ -0,0 +1,9 @@
+#ifndef DynamicCPP_folly_dynamic_included
+#define DynamicCPP_folly_dynamic_included
+
+#include <vector>
+#include <string>
+
+void doFollyDynamic(std::vector<std::string>);
+
+#endif // DynamicCPP_folly_dynamic_included
